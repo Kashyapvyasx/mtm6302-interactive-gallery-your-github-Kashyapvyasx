3e5ea8d89296f98b2def5e5018400509129f5eff
kashyapvyas <vyas0021@algonquinlivel.com>
2023-11-24 00:47:59 -0500

    Completes the assignment

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# mtm6302-interactive-gallery-your-github-Kashyapvyasx
+## Student Number : 41068127
+## Student Namne : Kashyap Vyas
+
+#### Motorcycle Image Collection from Pexels ####
+
+## Photographer: Nikita Khandelwal
+## Description: A detailed view of a white and black bobber bike.
+## Source: https://www.pexels.com/photo/white-and-black-bobber-bike-819805/
+
+## Photographer: Pragyan Bezbaruah
+## Description: A lone motorcycle captured on an open road.
+## Source:  https://www.pexels.com/photo/motorcycle-in-the-middle-of-the-road-1715193/
+
+## Photographer: Javier Aguilera
+## Description: A sleek black motorcycle.
+## Source: https://www.pexels.com/photo/black-motorcycle-2611675/
+
+## Photographer: Javier Aguilera
+## Description: A Harley Davidson parked on a gravel road.
+## Source: https://www.pexels.com/photo/
+
+## Photographer: Norbert Norbi
+## Description: A motorcycle parked beside lush garden plants.
+## Source: https://www.pexels.com/photo/black-motorcycle-parked-beside-garden-plants-5023257/
+
+## Photographer: Valeria Boltneva
+## Description: A Harley Davidson VRSC parked elegantly.
+## Source: https://www.pexels.com/photo/a-parked-harley-davidson-vrsc-13336569/
+
+## Photographer: Андрей
+## Description: A Suzuki blue sports bike showcasing its dynamic design.
+## Source: https://www.pexels.com/photo/suzuki-blue-sports-bike-8194537/
+
+## Photographer: Akash A G
+## Description: A vibrant red and black big bike.
+## Source: https://www.pexels.com/photo/photo-of-a-red-and-black-big-bike-12801272/
+
+## Photographer: OĞUZHAN YAVUZ
+## Description: A red and black motorcycle parked on a city street.
+## Source: https://www.pexels.com/photo/a-red-and-black-motorcycle-parked-on-the-street-14155432/ 
+
+## Photographer: Ankit Sihag
+## Description: A Honda CB300R parked on an asphalt road.
+## Source: https://www.pexels.com/photo/a-parked-honda-cb300r-on-the-road-6191490/
+
+## Photographer: mali maeder
+## Description: A stylish black Ducati cafe racer.
+## Source: https://www.pexels.com/photo/black-ducati-cafe-racer-motorcycle-112473/
+
+## Photographer: Pixabay
+## Description: A dynamic black and blue sports bike.
+## Source:  https://www.pexels.com/photo/black-and-blue-sports-bike-40904/
---
+++ script.js
@@ -0,0 +1,77 @@
+let images =  [
+    {
+        url: "images/pexels-akash-a-g-12801272.jpg",
+        description: "TVS Apache RTR 310"
+    },
+    {
+        url: "images/pexels-ankit-sihag-6191490.jpg",
+        description: "Bajaj Dominar 400"
+    },
+    {
+        url: "images/pexels-javier-aguilera-2611654.jpg",
+        description: " Harley-Davidson Street 750"
+    },
+    {
+        url: "images/pexels-javier-aguilera-2611675.jpg",
+        description: "Harley-Davidson Sportster Iron 883"
+    },
+    {
+        url: "images/pexels-mali-maeder-112473.jpg",
+        description: "Ducati 900 Supersport"
+    },
+    {
+        url: "images/pexels-nikita-khandelwal-819805.jpg",
+        description: "Triumph Bonneville"
+    },
+    {
+        url: "images/pexels-norbert-norbi-5023257.jpg",
+        description: "Harley-Davidson Custom Cruiser line"
+    },
+    {
+        url: "images/pexels-oğuzhan-yavuz-14155432.jpg",
+        description: "Honda CBR600RR"
+    },
+    {
+        url: "images/pexels-pixabay-40904.jpg",
+        description: "BMW K1300S"
+    },
+    {
+        url: "images/pexels-pragyan-bezbaruah-1715193.jpg",
+        description: "Triumph Thruxton"
+    },
+    {
+        url: "images/pexels-valeria-boltneva-13336569.jpg",
+        description: "Harley-Davidson V-Rod"
+    },
+    {
+        url: "images/pexels-андрей-8194537.jpg",
+        description: "Suzuki GSX-R1000"
+    }
+];
+
+let gallery = document.getElementById("gallery");
+let modal = document.getElementById("modal");
+let modalImageContainer = document.getElementById("modal-img-container");
+let modalImage = document.getElementById("modal-img");
+let modalDescription = document.getElementById("modal-description");
+
+for (let i = 0; i < images.length; i++) {
+    let img = document.createElement("img");
+    img.src = images[i].url;
+    img.alt = images[i].description;
+    gallery.appendChild(img);
+}
+
+gallery.addEventListener("click", function(event) {
+    if (event.target.tagName === "IMG") {
+        modal.style.display = "flex";
+        modalImage.src = event.target.src;
+        modalDescription.textContent = event.target.alt;
+    }
+});
+
+let closeButton = document.getElementById("close");
+
+closeButton.addEventListener("click", function() {
+  modal.style.display = "none";
+});
